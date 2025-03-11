addb13271d682073b51dcadb724a9ae341396483
Yuvraj-Sood <101963920+Yuvraj-Sood@users.noreply.github.com>
2025-03-11 10:29:14 +0530

    Add files via upload

---
+++ C#/Armstrong/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Armstrong
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, r, sum=0, t;
+            Console.WriteLine("Enter a Number : ");
+            n=Int32.Parse(Console.ReadLine());
+            t = n;
+            while(t>0)
+            {
+                r = t % 10;
+                sum = sum + (r * r * r);
+                t = t / 10;
+            }
+            if(sum==n)
+            {
+                Console.WriteLine("Armstrong Number");
+            }
+            else
+            {
+                Console.WriteLine("Not");
+            }
+            Console.ReadLine();
+        }
+    }
+}
