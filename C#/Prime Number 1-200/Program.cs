8c5a73b5f390c32a71002ba588ffdf9d0f6876c4
Yuvraj-Sood <101963920+Yuvraj-Sood@users.noreply.github.com>
2025-03-11 10:31:54 +0530

    Add files via upload

---
+++ C#/Prime Number 1-200/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime_Number_1_200
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int i,j, c = 0;
+            Console.Write("Prime Numbers From 1-200 are : ");
+            for(i=1;i<=200;i++)
+            {
+                int t = i;
+                c = 0;
+                int sq = (int)(Math.Sqrt(t));
+                for (j = 2; j <= sq; j++)
+                {
+                    if (t % j == 0)
+                    {
+                        c++;
+                        break;
+                    }
+                }
+                if (c == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ C#/Prime Number/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prime_Number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a Number : ");
+            int n=Int32.Parse(Console.ReadLine());
+            int sq=(int)(Math.Sqrt(n));
+            int i,c=0;
+            for(i=2;i<=sq; i++)
+            {
+                if(n%i==0)
+                {
+                    c++;
+                    break;
+                }
+            }
+            if(c==0)
+            {
+                Console.WriteLine("Prime Number");
+            }
+            else
+            {
+                Console.WriteLine("Not");
+            }
+            Console.ReadLine(); 
+        }
+    }
+}
